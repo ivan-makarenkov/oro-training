b47bd580fb45266f6e8f792f3d48867a8375ad29
Ivan Makarenkov <info@ivmak.com>
2021-04-30 03:59:06 +0300

    Service decoration

---
+++ src/Training/Bundle/UserNamingBundle/Provider/EntityNameProviderDecorator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\Bundle\UserNamingBundle\Provider;
+
+use Oro\Bundle\EntityBundle\Provider\EntityNameProviderInterface;
+use Oro\Bundle\LocaleBundle\Model\FullNameInterface;
+
+class EntityNameProviderDecorator implements EntityNameProviderInterface
+{
+    protected EntityNameProviderInterface $entityNameProvider;
+
+    public function __construct(EntityNameProviderInterface $entityNameProvider)
+    {
+        $this->entityNameProvider = $entityNameProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName($format, $locale, $entity): string
+    {
+        if ($entity instanceof FullNameInterface) {
+            return sprintf(
+                '%s %s %s',
+                $entity->getLastName(),
+                $entity->getFirstName(),
+                $entity->getMiddleName()
+            );
+        }
+
+        return $this->entityNameProvider->getName($format, $locale, $entity);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNameDQL($format, $locale, $className, $alias): string
+    {
+        return $this->entityNameProvider->getNameDQL($format, $locale, $className, $alias);
+    }
+}
