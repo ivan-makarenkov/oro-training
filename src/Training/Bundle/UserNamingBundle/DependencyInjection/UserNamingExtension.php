b4451db6291af5908a2dee25da3ec6324213feaa
Ivan Makarenkov <info@ivmak.com>
2021-04-30 04:44:33 +0300

    User view block

---
+++ src/Training/Bundle/UserNamingBundle/DependencyInjection/UserNamingExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\Bundle\UserNamingBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class UserNamingExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
